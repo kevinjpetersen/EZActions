cd68d6c50d4978926a046b1007524b848569538d
Kevin Petersen <kjp@simplyture.com>
2018-03-23 03:16:45 +0100

    Init

---
+++ EZActions/EZActionsCommand.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace EZActions
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class EZActionsCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("e3357728-8707-4de5-9c61-56d506bedef7");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EZActionsCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private EZActionsCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static EZActionsCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new EZActionsCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            //string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            //string title = "EZActionsCommand";
+            try
+            {
+                EZActionsController ez = new EZActionsController(ServiceProvider);
+                ez.Generate();
+            }
+            catch { }
+            //Thread thread = new Thread(() =>
+            //{
+                
+            //});
+            //thread.IsBackground = true;
+            //thread.Start();
+            
+            //// Show a message box to prove we were here
+            //VsShellUtilities.ShowMessageBox(
+            //    this.ServiceProvider,
+            //    message,
+            //    title,
+            //    OLEMSGICON.OLEMSGICON_INFO,
+            //    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            //    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ EZActions/EZActionsController.cs
@@ -0,0 +1,134 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Design;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZActions
+{
+    public class EZActionsController
+    {
+        private IServiceProvider provider;
+
+        public EZActionsController(IServiceProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public List<Type> GetAllTypes()
+        {
+            var trs = GetTypeDiscoveryService();
+            var types = trs.GetTypes(typeof(object), true /*excludeGlobalTypes*/);
+            var result = new List<Type>();
+            foreach (Type type in types)
+            {
+                if (type.IsPublic)
+                {
+                    if (!result.Contains(type))
+                        result.Add(type);
+                }
+            }
+            return result;
+        }
+
+        private ITypeDiscoveryService GetTypeDiscoveryService()
+        {
+            var dte = GetService<EnvDTE.DTE>();
+            var typeService = GetService<DynamicTypeService>();
+            var solution = GetService<IVsSolution>();
+            IVsHierarchy hier;
+            var projects = dte.ActiveSolutionProjects as Array;
+            var currentProject = projects.GetValue(0) as Project;
+            solution.GetProjectOfUniqueName(currentProject.UniqueName, out hier);
+            return typeService.GetTypeDiscoveryService(hier);
+        }
+
+        private T GetService<T>()
+        {
+            return (T)provider.GetService(typeof(T));
+        }
+
+        public void Generate()
+        {
+            try
+            {
+                if(provider == null)
+                {
+                    ShowMessage("It doesn't seem like you have any MVC Project open. Please open an MVC Project, then try again!", OLEMSGICON.OLEMSGICON_WARNING);
+                    return;
+                }
+
+                DTE dte = (DTE)provider.GetService(typeof(DTE));
+
+                CodeElements ce = dte.Solution.Projects.Item(1).CodeModel.CodeElements;
+
+
+                if(dte == null || dte.Solution == null || dte.Solution.IsOpen == false)
+                {
+                    ShowMessage("It doesn't seem like you have any MVC Project open. Please open an MVC Project, then try again!", OLEMSGICON.OLEMSGICON_WARNING);
+                    return;
+                }
+
+                string solutionDir = System.IO.Path.GetDirectoryName(dte.Solution.FullName);
+                StringBuilder ezActionsScript = new StringBuilder();
+
+                List<Type> types = GetAllTypes().Where(p => p.BaseType != null && p.BaseType.Name == "Controller").ToList();
+                List<string> errors = new List<string>();
+                
+                foreach(Type controller in types)
+                {
+                    try
+                    {
+                        ezActionsScript.AppendLine(controller.Name);
+
+                        foreach (MethodInfo mi in controller.GetMethods(BindingFlags.Instance | BindingFlags.Public).Where(p => p.IsSpecialName == false && !typeof(object).GetMethods().Select(me => me.Name).Contains(p.Name) && p.Name != "Dispose"))
+                        {
+                            ezActionsScript.AppendLine("- " + mi.Name + "(" + (mi.GetParameters() != null ? string.Join(",", mi.GetParameters().Select(p => p.Name)) : "None") + "), Return: " + mi.ReturnType.Name);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        errors.Add(controller.Name + " failed to generate because of following error: " + e.Message + ", " + e.StackTrace);
+                    }
+                }
+
+                if(errors.Count > 0)
+                {
+                    File.WriteAllText(solutionDir + "/Errors.txt", string.Join("\n", errors));
+                }
+
+                File.WriteAllText(solutionDir + "/EZActions.js", ezActionsScript.ToString());
+
+
+                ShowMessage("Generated successfully!");
+            }
+            catch(Exception e)
+            {
+                ShowMessage("An error occured while generating: " + e.Message + ", " + e.StackTrace, OLEMSGICON.OLEMSGICON_CRITICAL);
+            }
+        }
+
+        private void ShowMessage(string message, OLEMSGICON icon = OLEMSGICON.OLEMSGICON_INFO)
+        {
+            try
+            {
+                VsShellUtilities.ShowMessageBox(
+                    provider,
+                    message,
+                    "Message from EZActions:",
+                    icon,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+            catch { }
+        }
+    }
+}
